ea1c9795ed1ef12a1821b06c90737d8bea31cc19
Raghav Verma <142114433+vV29Raghav@users.noreply.github.com>
2025-02-18 05:48:40 +0000

    some spacing

--- Day1_JS/16_02.js
+++ Day1_JS/16_02.js
@@ -1,9 +1,17 @@
 console.log("Raghav");
+
 const id=144;
+
 let accountEmail="Raghav@gmail";
+
 var pass="12343";
+
 city="Haryana";
+
 let x;//it is undefined
+
 //Print the whole thing in tabular format
+
 //var not use->to stay away from block and functional scope problems
+
 console.table([id,accountEmail,pass,city]);
