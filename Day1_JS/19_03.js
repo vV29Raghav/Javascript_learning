657b80a6fff2ea8675f46a85e0ce891175bd6056
Raghav Verma <142114433+vV29Raghav@users.noreply.github.com>
2025-03-21 09:10:16 +0000

    DOM start

---
+++ Day1_JS/19_03.js
@@ -0,0 +1,11 @@
+//Window object -> Document -> HTML -> Head and Body
+//console.log(document.links)->it return html collection of all the links present on website
+
+document.getElementById('idname');//access element by id
+document.getElementById('idname').innerHTML="<h2/>Raghav";//access element by id and change its value
+
+//setAttribute can override our attributes by giving value to its attribute ans return undefined in console but change
+//To change style  attribute.style.change
+// document.getElementByClassName('bg').getAttribute()->to get attribute and set attribute value
+//Differnece between textContent and innerText is textContent has ability to show hidden or non visible text in part hide by style while innerHtml show whole html part inside that id
+
