4ce4949611ee873d8a2420e89d89b0f9ee93ff52
Raghav Verma <142114433+vV29Raghav@users.noreply.github.com>
2025-03-22 12:49:17 +0000

    DOM manipulation with parent,child,create element

--- Day1_JS/19_03.js
+++ Day1_JS/19_03.js
@@ -3,9 +3,19 @@
 
 document.getElementById('idname');//access element by id
 document.getElementById('idname').innerHTML="<h2/>Raghav";//access element by id and change its value
+document.getElementsByClassName('idname');//return html collection
+
 
 //setAttribute can override our attributes by giving value to its attribute ans return undefined in console but change
 //To change style  attribute.style.change
 // document.getElementByClassName('bg').getAttribute()->to get attribute and set attribute value
 //Differnece between textContent and innerText is textContent has ability to show hidden or non visible text in part hide by style while innerHtml show whole html part inside that id
 
+document.querySelector('h1')
+document.querySelector('#h1')//id selection
+document.querySelector('.h1')//classs selection
+document.querySelector('input[type="password"]')//it will retunr input type of password
+document.querySelector('p:first-child');
+const p=document.querySelectorAll('h1');//here it return nodelist on which we can apply ofr loop and access element by p[0];  
+
+//Conevrt html collection to array using Array.from(HTML collections);  
