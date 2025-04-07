51f341f8a65f48bfa7c936c28163582e7ffe2abf
Raghav Verma <142114433+vV29Raghav@users.noreply.github.com>
2025-04-07 17:44:05 +0000

    Async something

---
+++ Day1_JS/7_04.js
@@ -0,0 +1,7 @@
+// Javascript is synchronous, single threaded
+
+//Block code-Read file synchronous and block code till running and Non blocking code is vice-versa.
+
+//Register call back store info about set timeout,set interval and store in call stack afetr getting through taskQueue
+
+//fetch() function have promises  having something high priority queue which aslo add function calls to call stack
